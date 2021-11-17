c9e82c462c953f21a0dd8f256ea844d0de5ee4b2
Yuqing Wu <ywu20@stuy.edu>
2021-11-17 12:57:54 -0500

    fixed errors

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
-// fork() returns child's pid.
+
 #include<stdio.h>
 #include<unistd.h>
 #include<time.h>
 #include<stdlib.h>
 #include<sys/wait.h>
 int main(){
-
- // fork first.
+srand(time(NULL));
  int child1 = fork();
  int child2 = 0;
 int w, status;
-
+ 
 if(child1){
  printf("Parent Message\n");
  child2 = fork();
@@ -18,24 +17,17 @@ if(child1){
 
 if(child1 == 0 || child2 == 0){
   printf("Child %d starts\n", getpid());
-  srand(time(NULL));
+ 
   int t = rand() %4 + 2;
   sleep(t);
-  printf("Finish child: %d\n",getpid());
+  printf("Finish child: %d ,%d\n",getpid(),t);
+  return t;
 }else{
-
+for(int i=0;i<2;i++){
   w = wait(&status);
   printf("Child %d slept for %d\n", w, WEXITSTATUS(status));
+  }
   printf("End parent\n");
 }
-
-/*
- else{
-  printf("parent process initial message...\n");
-  childpid = fork();
-  w = wait (&status); // returns child's pid.
-  printf("");
- }
- */
 
 }
