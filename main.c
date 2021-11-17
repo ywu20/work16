e478d3cc0fdb1921b6efb991a733aed617046d27
Yuqing Wu <ywu20@stuy.edu>
2021-11-17 08:34:39 -0500

    hw

--- main.c
+++ main.c
@@ -3,20 +3,39 @@
 #include<unistd.h>
 #include<time.h>
 #include<stdlib.h>
+#include<sys/wait.h>
 int main(){
 
- /. fork first. 
- int childpid = 1;
- int wait, status;
+ // fork first.
+ int child1 = fork();
+ int child2 = 0;
+int w, status;
 
-if(childpid == 0){
- printf("print child pid: %d\n", childpid);
- sleep(2); // change to random sec later
- }else{
+if(child1){
+ printf("Parent Message\n");
+ child2 = fork();
+ }
+
+if(child1 == 0 || child2 == 0){
+  printf("Child %d starts\n", getpid());
+  srand(time(NULL));
+  int t = rand() %4 + 2;
+  sleep(t);
+  printf("Finish child: %d\n",getpid());
+}else{
+
+  w = wait(&status);
+  printf("Child %d slept for %d\n", w, WEXITSTATUS(status));
+  printf("End parent\n");
+}
+
+/*
+ else{
   printf("parent process initial message...\n");
   childpid = fork();
-  w = wait (&status); // returns child's pid. 
+  w = wait (&status); // returns child's pid.
   printf("");
  }
- 
+ */
+
 }
